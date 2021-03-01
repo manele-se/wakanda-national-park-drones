c5ba28efa30c233a8758091c37d0ab326c615e95
Dorothy1227 <1612926@mail.nankai.edu.cn>
2021-03-01 02:03:57 +0100

    implement travel function of rangers

--- human/src/ranger/Ranger.java
+++ human/src/ranger/Ranger.java
@@ -4,9 +4,11 @@ import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.json.JSONObject;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 //A component (implemented as a separate process) that ‘simulates’ one (or more) person(s):
@@ -42,33 +44,70 @@ public class Ranger {
         }
     }
 
-    public void travel(){
+    public void travel() {
         Random r = new Random();
 
         // random generate latitude
         int i1 = r.nextInt(10);
         double d1 = r.nextDouble();
-        double x1 = i1*(10 - d1);
+        double x1 = i1 * (10 - d1);
         System.out.println(x1);
-        BigDecimal temp   =   new   BigDecimal(x1);
-        double   x   =  temp.setScale(2, RoundingMode.HALF_UP).doubleValue();
-        this. x = x;
+        BigDecimal temp = new BigDecimal(x1);
+        x1 = temp.setScale(2, RoundingMode.HALF_UP).doubleValue();
+        this.x = x1;
 
         // random generate longitude
         int i2 = r.nextInt(10);
         double d2 = r.nextDouble();
-        double y1 = i2*(10 + d2);
+        double y1 = i2 * (10 + d2);
         System.out.println(y1);
-        temp   =   new   BigDecimal(x1);
-        double   y   =  temp.setScale(2, RoundingMode.HALF_UP).doubleValue();
-        this.y = y;
+        temp = new BigDecimal(y1);
+        y1 = temp.setScale(2, RoundingMode.HALF_UP).doubleValue();
+        this.y = y1;
 
+        // travel around the park
+        int k = 0;
+        while (true) {
+            int t = r.nextInt(2);
 
+            // around, maybe not needed
+            if (k == 0) {
+                x = x + 10.0;
+                k += 1;
+            } else if (k == 1) {
+                y = y + 10.0;
+                k += 1;
+            } else if (k == 2) {
+                x = x - 10.0;
+                k += 1;
+            } else {
+                y = y - 10.0;
+                k = 0;
+            }
 
-        System.out.println(x);
+            //random send information
+            if (t == 1) {
+                try {
+                    sendInformation();
+                    //System.out.println("latitude:" + x + "longitude" + y);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-    // send signal
-    public void sendInformation() {
 
+    // send signal
+    public void sendInformation() throws MqttException {
+        JSONObject positionToSend = new JSONObject();
+        positionToSend.put("latitude", this.x);
+        positionToSend.put("longitude", this.y);
+
+        String sendThisJson = positionToSend.toString();
+        byte[] sendTheseBytes = StandardCharsets.UTF_8.encode(sendThisJson).array();
+
+        String thisRangerIdentity = this.name;
+        String thisRangerLocationTopic = "chalmers/dat220/group1/ranger/" + thisRangerIdentity + "/location";
+        client.publish(thisRangerLocationTopic, sendTheseBytes, 0, false);
     }
 }
